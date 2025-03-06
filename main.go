12a0f32ac641ffe2ddced5f1d29b0cbe9619fc30
AlexGochu <gochualex@gmail.com>
2025-03-06 00:10:05 +0000

    Adds additional logic about response and request functionality

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("Hello World")
+		fmt.Fprintln(writer, "Hello World")
+		d, err := ioutil.ReadAll(request.Body)
+		if err != nil {
+			http.Error(writer, "Ooopsª"+err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		log.Println(string(d))
+		fmt.Fprintln(writer, string(d))
+	})
+
+	http.HandleFunc("/goodbye", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("Goodbye World")
+		fmt.Fprintln(writer, "Goodbye World")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
